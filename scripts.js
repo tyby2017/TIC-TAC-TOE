f097094957b2b911f48995017ea1e75bd530f6ed
Tiberiu <tiberiu.miinea@gmail.com>
2021-04-03 18:09:14 +0300

    Added deciding winner alg

--- scripts.js
+++ scripts.js
@@ -2,6 +2,17 @@ const cells = [...document.querySelectorAll('.cell')];
 let xTurn = true;
 const X = 'X';
 const O = 'O';
+const WINNING_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 
 cells.forEach(cell => {
     cell.addEventListener('click', handleClick, {once:true});
@@ -9,6 +20,44 @@ cells.forEach(cell => {
 
 function handleClick(e) {
     const cell = e.target;
-    xTurn ? cell.innerHTML = X : cell.innerHTML = O;
+    const currentMark = xTurn ? X : O;
+    cell.innerHTML = currentMark;
+    const currentMarks = [];
+    cells.forEach(cell => {
+        if(cell.innerHTML === currentMark)
+        {
+            currentMarks.push(Number(cell.getAttribute('id')));
+        }
+    });
+
+    if(checkWin(currentMarks)) {
+        console.log('Winner', currentMark);
+    }
     xTurn = !xTurn;
 }
+
+
+function checkWin(currentMarks) {
+        for(let i=0;i<WINNING_COMBINATIONS.length;i++) {
+            let winner = true;
+            for(let j=0;j<WINNING_COMBINATIONS[i].length;j++)
+            {
+                if(!currentMarks.includes(WINNING_COMBINATIONS[i][j]))
+                {
+                    winner = false;
+                }
+            }
+            if(winner){
+                return true;
+            }            
+        }        
+}
+
+
+
+
+
+
+
+
+
